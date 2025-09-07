3d044c64e752c02af16bc005c1b50e92b462f929
zow.py <logantrilator@gmail.com>
2025-09-07 22:17:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,6 @@
+// Point d'entrée — démarre simplement le bot
 require('dotenv').config({ path: 'id.env' });
-const fs = require('fs');
-const path = require('path');
-const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
 
-const BOT_TOKEN = process.env.BOT_TOKEN;
+const { startBot } = require('./src/bot');
 
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
-  partials: [Partials.Channel],
-});
-
-client.commands = new Collection();
-
-// Charger commandes
-const foldersPath = path.join(__dirname, 'src/commands');
-for (const folder of fs.readdirSync(foldersPath)) {
-  const commandsPath = path.join(foldersPath, folder);
-  for (const file of fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'))) {
-    const command = require(path.join(commandsPath, file));
-    client.commands.set(command.data.name, command);
-  }
-}
-
-// Charger events
-const eventsPath = path.join(__dirname, 'src/events');
-for (const file of fs.readdirSync(eventsPath).filter(f => f.endsWith('.js'))) {
-  const event = require(path.join(eventsPath, file));
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(client, ...args));
-  } else {
-    client.on(event.name, (...args) => event.execute(client, ...args));
-  }
-}
-
-client.login(BOT_TOKEN);
+startBot();
