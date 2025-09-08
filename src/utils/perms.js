be8341e75c33a696c28355ddc1c3fe10ff2afc59
zow.py <logantrilator@gmail.com>
2025-09-08 04:55:39 +0200

    Update perms.js

--- src/utils/perms.js
+++ src/utils/perms.js
@@ -1,4 +1,3 @@
-// Qui peut PARLER au joueur (répondre dans les salons DM)
 const TALK_ROLES = [
   process.env.HELPER_ROLE_ID,
   process.env.MODO_ROLE_ID,
@@ -11,7 +10,6 @@ const TALK_ROLES = [
   process.env.FONDA_ROLE_ID,
 ].filter(Boolean);
 
-// Qui peut SUPPRIMER un ticket (en plus de close)
 const DELETE_ROLES = [
   process.env.ADMIN_ROLE_ID,
   process.env.SUPADMIN_ROLE_ID,
@@ -23,8 +21,7 @@ const DELETE_ROLES = [
 ].filter(Boolean);
 
 function memberHasAnyRole(member, roleIds) {
-  if (!member) return false;
-  return member.roles.cache.some(r => roleIds.includes(r.id));
+  return !!member?.roles?.cache?.some(r => roleIds.includes(r.id));
 }
 
 module.exports = { TALK_ROLES, DELETE_ROLES, memberHasAnyRole };
