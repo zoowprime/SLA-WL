16af5d1e6b83cb2a9d92f396d9aec8bcfa872c12
zow.py <logantrilator@gmail.com>
2025-09-08 03:10:19 +0200

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,16 @@
+const { ActivityType } = require('discord.js');
+
 module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
     console.log(`[SLA] Connecté en tant que ${client.user.tag}`);
+
+    client.user.setPresence({
+      activities: [
+        { name: 'DM POUR SUPPORT', type: ActivityType.Playing }
+      ],
+      status: 'online'
+    });
   },
 };
