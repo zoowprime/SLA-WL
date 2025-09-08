ae67b4ab14762c2a47984a5e1ac8fa092c7afdf7
zow.py <logantrilator@gmail.com>
2025-09-08 04:28:06 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
 
-const BOT_TOKEN = process.env.BOT_TOKEN;
-
 function startBot() {
   const client = new Client({
     intents: [
@@ -11,45 +9,24 @@ function startBot() {
       GatewayIntentBits.GuildMembers,
       GatewayIntentBits.GuildMessages,
       GatewayIntentBits.MessageContent,
-      GatewayIntentBits.DirectMessages
+      GatewayIntentBits.DirectMessages, // 👈 nécessaire pour DM
     ],
     partials: [Partials.Channel, Partials.Message, Partials.Reaction, Partials.User, Partials.GuildMember],
   });
 
   client.commands = new Collection();
 
-  // Charger les commandes
-  const foldersPath = path.join(__dirname, 'commands');
-  const commandFolders = fs.readdirSync(foldersPath);
-
-  for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'));
-
-    for (const file of commandFiles) {
-      const filePath = path.join(commandsPath, file);
-      const command = require(filePath);
-      if ('data' in command && 'execute' in command) {
-        client.commands.set(command.data.name, command);
-      }
-    }
-  }
-
-  // Charger les events
+  // EVENTS
   const eventsPath = path.join(__dirname, 'events');
-  const eventFiles = fs.readdirSync(eventsPath).filter(f => f.endsWith('.js'));
-
-  for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-    if (event.once) {
-      client.once(event.name, (...args) => event.execute(client, ...args));
-    } else {
-      client.on(event.name, (...args) => event.execute(client, ...args));
+  if (fs.existsSync(eventsPath)) {
+    for (const file of fs.readdirSync(eventsPath).filter(f => f.endsWith('.js'))) {
+      const ev = require(path.join(eventsPath, file));
+      if (ev.once) client.once(ev.name, (...args) => ev.execute(client, ...args));
+      else client.on(ev.name, (...args) => ev.execute(client, ...args));
     }
   }
 
-  client.login(BOT_TOKEN);
+  client.login(process.env.BOT_TOKEN);
 }
 
 module.exports = { startBot };
